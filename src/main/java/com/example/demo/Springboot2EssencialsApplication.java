044580556ae3fbb9304686035d0fe11a382d1140
Italo Teixeira de Oliveira <italo.oliveira-for@MVFOR.local>
2021-02-18 20:16:10 -0300

    Gerando imagem docker da aplicação

--- src/main/java/com/example/demo/Springboot2EssencialsApplication.java
+++ src/main/java/com/example/demo/Springboot2EssencialsApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 public class Springboot2EssencialsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot2EssencialsApplication.class, args);
+	}
+	
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+	    @Value("${spring.application.name}") String applicationName) {
+	    return (registry) -> registry.config().commonTags("application", applicationName);
 	}
 
 }
